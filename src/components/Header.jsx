4f01eb604317a3d1fd7880dea200d439dd50b3f6
Fahim Imtiaz <31901239+whichfahim@users.noreply.github.com>
2022-08-08 17:42:02 +0600

    Added profile photo
    Readjusted font sizes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,12 @@
-export default function Header(){
-    return(
-        <div className="section-header">
-            {/* <a href="https://unsplash.com/@benjaminsweet?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ben Sweet</a> on <a href="https://unsplash.com/s/photos/profile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
-            <img src="src\assets\images\profile.png" className="profile" alt="Profile Photo" />
-        </div>
-        
-    )
+export default function Header() {
+  return (
+    <div className="section-header">
+      {/* <a href="https://unsplash.com/@benjaminsweet?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ben Sweet</a> on <a href="https://unsplash.com/s/photos/profile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a> */}
+      <img
+        src="src\assets\images\6238.jfif"
+        className="profile"
+        alt="Profile Photo"
+      />
+    </div>
+  );
 }
-
